06291fb5bf6ace967544cb04e2e477ca37ac4499
ebridge2 <ebridge2@jhu.edu>
2016-08-04 17:09:50 -0400

    added kalman filtering automatically

--- data_processing/Rutils/check_mnr.R
+++ data_processing/Rutils/check_mnr.R
@@ -26,7 +26,7 @@ source('obs2kf.R')
 
 dataset <- 'NKI'
 outpath <- 'C:/Users/ebrid/Documents/GitHub/ugrad-data-design-team-0/reveal/images/week_81/'
-opts <- 'kf'
+kfopts <- c('kf', 'no kf')
 ## Loading Timeseries --------------------------------------------------------------------------------
 gpath <- paste('C:/Users/ebrid/Documents/R/FNGS_results/for_foo/', dataset, '/', sep="")
 tsnames <- list.files(gpath, pattern="\\.rds", full.names=TRUE)
@@ -36,46 +36,60 @@ tsobj <- open_timeseries(tsnames, scan_pos=2)
 ts <- tsobj[[1]]
 sub <- tsobj[[2]]
 
-zsc <- signal2zscore(ts)
-kf <- obs2kf(ts)
-corr <- obs2corr(kf)
 
 
-## Change Convention from preferred vara[[sub]][array] to vara[sub,array] for use with old code ---------
-nroi <- dim(corr[["1"]])[1]
-nscans <- length(corr)
-wgraphs <- array(rep(NaN, nroi*nroi*nscans), c(nroi, nroi, nscans))
+maxmnr <- 0
 
-counter <- 1
-for (subject in names(corr)) {
-  wgraphs[,,counter] <- corr[[subject]]
-  counter <- counter + 1
-}
-
-## Compute MNR ----------------------------------------------------------------
-
-thresh_obj <- thresh_mnr(wgraphs, sub)
-
-mnrthresh <- thresh_obj[[1]]
-Dthresh <- thresh_obj[[2]]
-
-ranked_graphs <- rank_matrices(wgraphs)
-Drank <- distance(ranked_graphs)
-mnrrank <- mnr(rdf(Drank, sub))
-
-Draw <- distance(wgraphs)
-mnrraw <- mnr(rdf(Draw, sub))
-maxmnr <- max(c(mnrraw, mnrrank, mnrthresh))
-
-if (isTRUE(all.equal(maxmnr, mnrraw))) {
-  Dmax <- Draw
-  winner <- 'raw'
-} else if (isTRUE(all.equal(maxmnr, mnrrank))) {
-  Dmax <- Drank
-  winner <- 'rank'
-} else {
-  Dmax <- Dthresh
-  winner <- 'thresh'
+for (opt in opts) {
+  if (isTRUE(all.equal(opt, 'kf'))) {
+    kf <- obs2kf(ts)
+    corr <- obs2corr(kf)
+    #zsc <- signal2zscore(ts)
+  } else {
+    corr <- obs2corr(ts)
+  }
+  
+  ## Change Convention from preferred vara[[sub]][array] to vara[sub,array] for use with old code ---------
+  nroi <- dim(corr[["1"]])[1]
+  nscans <- length(corr)
+  wgraphs <- array(rep(NaN, nroi*nroi*nscans), c(nroi, nroi, nscans))
+  
+  counter <- 1
+  for (subject in names(corr)) {
+    wgraphs[,,counter] <- corr[[subject]]
+    counter <- counter + 1
+  }
+  
+  ## Compute MNR ----------------------------------------------------------------
+  
+  thresh_obj <- thresh_mnr(wgraphs, sub)
+  
+  mnrthresh <- thresh_obj[[1]]
+  Dthresh <- thresh_obj[[2]]
+  
+  ranked_graphs <- rank_matrices(wgraphs)
+  Drank <- distance(ranked_graphs)
+  mnrrank <- mnr(rdf(Drank, sub))
+  
+  Draw <- distance(wgraphs)
+  mnrraw <- mnr(rdf(Draw, sub))
+  
+  optmax_mnr <- max(c(mnrraw, mnrrank, mnrthresh))
+  if (isTRUE(optmax_mnr > maxmnr)) {
+    maxmnr <- optmax_mnr
+    kfo <- opt
+    if (isTRUE(all.equal(maxmnr, mnrraw))) {
+      Dmax <- Draw
+      winner <- 'raw'
+    } else if (isTRUE(all.equal(maxmnr, mnrrank))) {
+      Dmax <- Drank
+      winner <- 'rank'
+    } else {
+      Dmax <- Dthresh
+      winner <- 'thresh'
+      
+    }    
+  }
 }
 
 ## Produce Plots for MNR --------------------------------------------------------
@@ -86,15 +100,17 @@ colnames(kde_dist) <- c("intra", "inter", "Graph Distance")
 meltkde <- melt(kde_dist, id="Graph Distance")
 colnames(meltkde) <- c("Graph Distance", "Relationship", "Probability")
 
+
+distance_title <- sprintf('MNR = %.4f, best = %s, %s', maxmnr, winner, kfo)
 distance_plot <- ggplot(melt(Dmax), aes(x=Var1, y=Var2, fill=value)) + 
   geom_tile(color="white") +
   scale_fill_gradientn(colours=c("darkblue","blue","purple","green","yellow"),name="distance") +
-  xlab("Scan") + ylab("Scan") + ggtitle(sprintf('MNR = %.4f, best = %s', maxmnr, winner)) +
+  xlab("Scan") + ylab("Scan") + ggtitle(distance_title) +
   theme(text=element_text(size=20))
 
 kde_plot <- ggplot()+geom_ribbon(data=meltkde, aes(x=`Graph Distance`, ymax=Probability, fill=Relationship), ymin=0, alpha=0.5) +
-  ggtitle("Intra and Inter Subject Relationships") + theme(text=element_text(size=20))
+  ggtitle("Subject Relationships") + theme(text=element_text(size=20))
 
-png(paste(outpath, dataset, "_", opts, ".png", sep=""), height=600, width = 1200)
+#png(paste(outpath, dataset, "_", opts, ".png", sep=""), height=600, width = 1200)
 multiplot(distance_plot, kde_plot, layout=matrix(c(1,2), nrow=1, byrow=TRUE))
-dev.off()
+#dev.off()
